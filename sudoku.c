f40d97cacba1e9d1e8e1f4997dfffc61757c344d
David Tauraso <david318@me.com>
2018-11-18 18:49:08 -0800

    Works for all 9 different subgrid threads

--- sudoku.c
+++ sudoku.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <pthread.h>
 
+
 int sudoku_grid[9][9] = {
 	{	7, 2, 6,    3, 5, 9,    4, 1, 8	},
 	{	4, 5, 8,    1, 6, 7,    2, 3, 9	},
@@ -18,6 +19,21 @@ int sudoku_grid[9][9] = {
 	{	2, 4, 1,    5, 9, 8,    6, 7, 3	}
 };
 
+/*
+int sudoku_grid[9][9] = {
+	{	7, 4, 6,    3, 5, 9,    4, 1, 8	},
+	{	4, 5, 8,    1, 4, 7,    2, 3, 9	},
+	{	9, 1, 3,    8, 2, 4,    7, 4, 5	},
+
+	{	1, 4, 2,    9, 7, 5,    3, 8, 4	},
+	{	3, 9, 4,    2, 4, 6,    1, 5, 7	},
+	{	8, 7, 5,    4, 1, 3,    9, 4, 6	},
+
+	{	5, 4, 7,    6, 4, 1,    8, 9, 2	},
+	{	6, 8, 9,    7, 4, 2,    5, 4, 1	},
+	{	2, 4, 1,    5, 9, 8,    6, 4, 3	}
+};
+*/
 bool check_col(int sudoku_grid[9][9], int column){
 	int returner[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
@@ -149,14 +165,14 @@ void* check_grid(void* stuff_from_pthread_create){
 
 	bool result = malloc(sizeof(bool));
 	int returner[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
+	subgrids->pass = 1;
 	//printf("%d\n", (GridStuct*) stuff_from_pthread_create->row_low_bound);
 
 	for (int i = subgrids->row_low_bound; i <= subgrids->row_high_bound; ++i)
 	{
 		for (int j = subgrids->col_low_bound; j <= subgrids->col_high_bound; ++j)
 		{
-			printf("%d\n", subgrids->sudoku_grid[i][j]);
+			//printf("%d\n", subgrids->sudoku_grid[i][j]);
 			if (1 == subgrids->sudoku_grid[i][j]){
 				returner[0] = 0;
 			}
@@ -190,14 +206,24 @@ void* check_grid(void* stuff_from_pthread_create){
 	{
 		if (returner[i] != 0){
 			//result = false;
-			subgrids->pass = 1;
-			printf("False!");
+			subgrids->pass = 0;
+
 			//return result;
 		}
 	}
+
+	if(subgrids->pass)
+	{
+		//subgrids->pass = 1;
+		printf("True!\n");
+
+	}
+	else
+	{
+		printf("False!\n");
+	}
 	//result = true;
-	subgrids->pass = 1;
-	printf("True!");
+
 	//return result;
 
 }
@@ -220,10 +246,18 @@ int main()
 	2, 5, 8
 
 
-	for(int i = 0; i < 3; i++)
-	{
-	
-}
+	0 2 0 2
+	0 2 3 5
+	0 2 6 8
+
+	3 5 0 2
+	3 5 3 5
+	3 5 6 8
+
+	6 8 0 2
+	6 8 3 5
+	6 8 6 8
+
 	bool grids_result = check_grid(sudoku_grid, 0, 2, 6, 8);
 	printf("%d\n", cols_result);
 	printf("%d\n", rows_result);
@@ -247,11 +281,15 @@ int main()
 	// Checking the grids
 	GridStuct* subgrids = malloc(sizeof(GridStuct) * 9);
 	//subgrids->sudoku_grid = sudoku_grid;
-	subgrids->row_low_bound = 0;
-	subgrids->row_high_bound = 2;
-	subgrids->col_low_bound = 6;
-	subgrids->col_high_bound = 8;
+	//subgrids->row_low_bound = 0;
+	//subgrids->row_high_bound = 2;
+	//subgrids->col_low_bound = 6;
+	//subgrids->col_high_bound = 8;
 
+	int row_low_bounds[9] = {0, 0, 0, 3, 3, 3, 6, 6, 6};
+	int row_high_bounds[9] = {2, 2, 2, 5, 5, 5, 8, 8, 8};
+	int col_low_bounds[9] = {0, 3, 6, 0, 3, 6, 0, 3, 6};
+	int col_high_bounds[9] = {2, 5, 8, 2, 5, 8, 2, 5, 8};
 	for(int k = 0; k < 9; k++)
 	{
 		for (int i = 0; i < 9; ++i)
@@ -261,8 +299,22 @@ int main()
 				subgrids[k].sudoku_grid[i][j] = sudoku_grid[i][j];
 			}
 		}
+		subgrids[k].row_low_bound = row_low_bounds[k];
+		subgrids[k].row_high_bound = row_high_bounds[k];
+		subgrids[k].col_low_bound = col_low_bounds[k];
+		subgrids[k].col_high_bound = col_high_bounds[k];
+		subgrids[k].pass = 0;
 	}
 
+
+
+
+
+
+	/*for(int i = 0; i < 9; i++)
+	{
+
+	}*/
 
 	/*
 	int num_args = argc - 1;
@@ -292,14 +344,14 @@ int main()
 	*/
 	for (int i = 0; i < 9; ++i)
 	{
-		pthread_create(&grid_threads[i], NULL, &check_grid, &subgrids[0]);
+		pthread_create(&grid_threads[i], NULL, &check_grid, &subgrids[i]);
 	}
 
 	for(int i = 0; i < 9; i++)
 	{
 		pthread_join(grid_threads[i], NULL);
 
-		printf("Sum is %d\n", subgrids[0].pass);
+		printf("resuld is %d\n", subgrids[i].pass);
 
 	}
 	return 0;
